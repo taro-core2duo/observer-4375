aa6d862c2f25325840a29383eb2ab9f6a176c458
taro <nsh@gmail.com>
2022-03-17 00:40:09 +0900

    rename img

--- src/scales-for-map.js
+++ src/scales-for-map.js
@@ -4,8 +4,8 @@ export const scales_for_map = [
     [30, {iconUrl:"/scale/3.png",iconSize:[50,50],iconAnchor:[25,25]}, 400],
     [40, {iconUrl:"/scale/4.png",iconSize:[50,50],iconAnchor:[25,25]}, 600],
     [45, {iconUrl:"/scale/5-.png",iconSize:[50,50],iconAnchor:[25,25]}, 800],
-    [50, {iconUrl:"/scale/5+.png",iconSize:[50,50],iconAnchor:[25,25]}, 1000],
+    [50, {iconUrl:"/scale/5plus.png",iconSize:[50,50],iconAnchor:[25,25]}, 1000],
     [55, {iconUrl:"/scale/6-.png",iconSize:[50,50],iconAnchor:[25,25]}, 1200],
-    [60, {iconUrl:"/scale/6+.png",iconSize:[50,50],iconAnchor:[25,25]}, 1400],
+    [60, {iconUrl:"/scale/6plus.png",iconSize:[50,50],iconAnchor:[25,25]}, 1400],
     [70, {iconUrl:"/scale/7.png",iconSize:[50,50],iconAnchor:[25,25]}, 1600]
 ]
